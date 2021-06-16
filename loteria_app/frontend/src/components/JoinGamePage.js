6e2e8e7be23ad64c9f8c3f9527fb607b09659ce6
valkar <valkarthecoder@gmail.com>
2021-06-16 11:11:06 -0700

    Added View for Join Game
    - added material ui components to join game
    - name is required to enter game
    - min length is 1 for field
    - added submit button and back button

--- loteria_app/frontend/src/components/JoinGamePage.js
+++ loteria_app/frontend/src/components/JoinGamePage.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography  from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+
+// from this page player can add their name and submit game code to join the running game.
+//  players can only be in 1 active game.
 
 export default class JoinGamePage extends Component {
     constructor(props) {
@@ -6,6 +16,34 @@ export default class JoinGamePage extends Component {
     }
 
     render() {
-        return <p>This is the page new players see when attempting to join a game.</p>;
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component="h4" variant="h4">
+                    Join Game
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl>
+                    <TextField required={ true } 
+                               type="text" 
+                               variant="outlined"
+                               label="Name"
+                               inputProps={{
+                                   min:1,
+                               }}
+                    />
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained">
+                    Join Game
+                </Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="secondary" variant="contained" to="/" component={ Link }>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>;
     }
 }
